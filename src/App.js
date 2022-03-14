adf1602b733f60d9c1d23375da494046bc7c7214
Quinton LaBorde <quintonlaborde@gmail.com>
2022-03-14 13:52:20 -0600

    added track info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Header from "./Components/Header.js";
 import Album from "./Components/Album.js";
+import Track from "./Components/Track";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="components">
         <Header />
         <Album />
+        <Track />
       </div>
     </div>
   );
---
+++ src/Components/Track.js
@@ -0,0 +1,12 @@
+import "./Track.scss";
+
+const Track = () => {
+  return (
+    <div className="track">
+      <p>Behold the Bearer of Light</p>
+      <p className="track__artist">Inferi</p>
+    </div>
+  );
+};
+
+export default Track;
