d9392fe246eddf9544b429229b85d20a72373ae1
Quinton LaBorde <quintonlaborde@gmail.com>
2022-03-22 11:10:08 -0600

    added control buttons

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -13,24 +13,26 @@ const Controls = () => {
   };
 
   return (
-    <div className="circle">
-      <span
-        className={`${pause ? "circle__btn" : "circle__btn shadow"}`}
-        onClick={clickHandler}
-      >
-        <div className={`${pause ? "pause" : "pause visibility"}`}>
-          <ion-icon name="pause"></ion-icon>
-        </div>
-        <div className={`${pause ? "play" : "play visibility"}`}>
-          <ion-icon name="play"></ion-icon>
-        </div>
-      </span>
-      <span
-        className={`${pause ? "circle__back-1" : "circle__back-1 paused"}`}
-      ></span>
-      <span
-        className={`${pause ? "circle__back-2" : "circle__back-2 paused"}`}
-      ></span>
+    <div className="buttonContainer">
+      <div className="icon__skipBack">
+        <ion-icon name="play-skip-back-outline"></ion-icon>
+      </div>
+      <div className="circle">
+        <span
+          className={`${pause ? "circle__btn" : "circle__btn shadow"}`}
+          onClick={clickHandler}
+        >
+          <div className={`${pause ? "pause" : "pause visibility"}`}>
+            <ion-icon className="icon" name="pause"></ion-icon>
+          </div>
+          <div className={`${pause ? "play" : "play visibility"}`}>
+            <ion-icon name="play"></ion-icon>
+          </div>
+        </span>
+      </div>
+      <div className="icon__skipForward">
+        <ion-icon name="play-skip-forward-outline"></ion-icon>
+      </div>
     </div>
   );
 };
