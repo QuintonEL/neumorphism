8cbeecdfc2ce641cad8167da4f311361eb6a7fe3
Quinton LaBorde <quintonlaborde@gmail.com>
2022-03-22 14:48:53 -0600

    started adding in dark mode

--- src/App.js
+++ src/App.js
@@ -5,9 +5,28 @@ import Track from "./Components/Track";
 import ProgressBar from "./Components/ProgressBar";
 import Controls from "./Components/Controls";
 
+import useLocalStorage from "use-local-storage";
+
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="container">
+    <div className="container" data-theme={theme}>
+      <div class="switch">
+        <div class="switch__1">
+          <input id="switch-1" type="checkbox" onClick={switchTheme} />
+          <label for="switch-1"></label>
+        </div>
+      </div>
       <div className="components">
         <Header />
         <Album />
